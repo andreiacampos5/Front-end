012585eed09b00193b0c1bf397af40ccabefec2e
andreacampos5 <andreiajosefa@prof.educacao.sp.gov.br>
2026-09-20 18:42:11 -0300

    Adiciona ALTERADO 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ALTERADO
+          Learn React ALTERADO 2
         </a>
       </header>
     </div>
